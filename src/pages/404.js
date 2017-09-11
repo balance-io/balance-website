c56473e47de79a5cdab6b2e69d9d1e217717b0cd
Pedro Gomes <pedrogomes94@gmail.com>
2017-09-11 15:44:36 +0100

    cleanup pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,7 @@ import React from 'react';
 const NotFoundPage = () => (
   <div>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <p>YOLO YOLO YOLO YOLOY </p>
+    <p>Nothing to see here</p>
   </div>
 );
 
