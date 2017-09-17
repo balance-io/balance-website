b5e71f89fa6ea07a9f962022dec7c7f8dd7731bd
Pedro Gomes <pedrogomes94@gmail.com>
2017-09-17 02:35:51 +0100

    update to react fiber

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-offline`
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-next`
   ]
 };
