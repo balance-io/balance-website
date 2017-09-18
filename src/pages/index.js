23829354861a3c4911fb9a8795b6f1c983d28639
Pedro Gomes <pedrogomes94@gmail.com>
2017-09-18 14:02:37 +0100

    fix server render

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Hero from '../sections/Hero';
 import BalanceOpen from '../sections/BalanceOpen';
 import { colors } from '../styles';
 
-const IndexPage = () => {
-  document.body.style.background = `rgb(${colors.darkerBlue})`;
-  return (
+class IndexPage extends Component {
+  componentDidMount() {
+    document.body.style.background = `rgb(${colors.darkerBlue})`;
+  }
+  render = () => (
     <div>
       <Hero />
       <BalanceOpen />
     </div>
   );
-};
-
+}
 export default IndexPage;
