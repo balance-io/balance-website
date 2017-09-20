574db36789d5db74f860ba27497fc7f79f4f2899
Pedro Gomes <pedrogomes94@gmail.com>
2017-09-20 16:46:47 +0100

    pop the video out of hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,97 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 import Hero from '../sections/Hero';
 import BalanceOpen from '../sections/BalanceOpen';
 import BalanceIOS from '../sections/BalanceIOS';
+import { colors, responsive } from '../styles';
 
-const IndexPage = () => (
-  <div>
-    <Hero />
-    <BalanceOpen />
-    <BalanceIOS />
-  </div>
-);
+const SVideoContainer = styled.div`
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  padding: 0 10%;
+  z-index: 1000;
+  background: rgba(${colors.dark}, 0.3);
+  transition: opacity 0.225s cubic-bezier(0, 0, 0.2, 1);
+  -webkit-backdrop-filter: blur(12px);
+  backdrop-filter: blur(12px);
+  opacity: ${({ show }) => (show ? 1 : 0)};
+  visibility: ${({ show }) => (show ? 'visible' : 'hidden')};
+  pointer-events: ${({ show }) => (show ? 'auto' : 'none')};
+  will-change: transform;
+  ${({ show }) =>
+    show &&
+    `
+    & > div {
+      transition: .225s cubic-bezier(0.19, 1, 0.22, 1);
+      transform: scale(1.0) translateY(0);
+    }
+  `};
+  @media screen and (${responsive.md.max}) {
+    padding: 0 5%;
+  }
+`;
+
+const SVideoWrapper = styled.div`
+  position: relative;
+  padding-bottom: 56.25%;
+  width: 100%;
+  margin: 0 auto;
+  height: 0;
+  will-change: transform;
+  background: #090c0f;
+  @media screen and (${responsive.sm.max}) {
+    padding-bottom: 177.77%;
+  }
+  & iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+class IndexPage extends Component {
+  state = {
+    showVideo: false
+  };
+  toggleVideo = () => {
+    const command = this.state.showVideo ? 'pauseVideo' : 'playVideo';
+    this.setState({ showVideo: !this.state.showVideo });
+    this.iframe.contentWindow.postMessage(`{"event":"command","func":"${command}","args":""}`, '*');
+  };
+  render = () => (
+    <div>
+      <Hero toggleVideo={this.toggleVideo} />
+      <BalanceOpen />
+      <BalanceIOS />
+      <SVideoContainer show={this.state.showVideo} onClick={this.toggleVideo}>
+        <SVideoWrapper>
+          <iframe
+            ref={c => (this.iframe = c)}
+            title="wefunder-youtube"
+            src={
+              typeof window !== 'undefined' ? window.innerWidth > 640 ? (
+                'https://www.youtube.com/embed/05w-S5gY0Y4?enablejsapi=1&showinfo=0&rel=0&color=white'
+              ) : (
+                'https://www.youtube.com/embed/c4UGoACmhUE?enablejsapi=1&showinfo=0&rel=0&color=white'
+              ) : null
+            }
+            allowFullScreen
+            frameBorder="0"
+          />
+        </SVideoWrapper>
+      </SVideoContainer>
+    </div>
+  );
+}
 
 export default IndexPage;
--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import SubscribeForm from '../components/SubscribeForm';
 import emailIcon from '../assets/images/email-icon.svg';
@@ -190,116 +191,38 @@ const STagline = styled.p`
   }
 `;
 
-const SVideoContainer = styled.div`
-  position: fixed;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  left: 0;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  padding: 0 10%;
-  z-index: 1000;
-  background: rgba(${colors.dark}, 0.3);
-  transition: opacity 0.225s cubic-bezier(0, 0, 0.2, 1);
-  -webkit-backdrop-filter: blur(12px);
-  backdrop-filter: blur(12px);
-  opacity: ${({ show }) => (show ? 1 : 0)};
-  visibility: ${({ show }) => (show ? 'visible' : 'hidden')};
-  pointer-events: ${({ show }) => (show ? 'auto' : 'none')};
-  will-change: transform;
-  ${({ show }) =>
-    show &&
-    `
-    & > div {
-      transition: .225s cubic-bezier(0.19, 1, 0.22, 1);
-      transform: scale(1.0) translateY(0);
-    }
-  `};
-  @media screen and (${responsive.md.max}) {
-    padding: 0 5%;
-  }
-`;
+const Hero = ({ toggleVideo }) => (
+  <SSection id="hero" minHeight={860} color={colors.fadedBlue} background={<SBackgroundImage />}>
+    <SSectionWrapper>
+      <SPreviewWrapper onClick={toggleVideo} mobile>
+        <SPreview mobile img={previewMobile}>
+          <SPlayButton mobile />
+        </SPreview>
+      </SPreviewWrapper>
 
-const SVideoWrapper = styled.div`
-  position: relative;
-  padding-bottom: 56.25%;
-  width: 100%;
-  margin: 0 auto;
-  height: 0;
-  will-change: transform;
-  background: #090c0f;
-  @media screen and (${responsive.sm.max}) {
-    padding-bottom: 177.77%;
-  }
-  & iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-`;
+      <SHalf>
+        <SSubscribe>
+          <SFormWrapper>
+            <img src={emailIcon} alt="email" />
+            <SForm />
+          </SFormWrapper>
 
-class Hero extends Component {
-  state = {
-    showVideo: false
-  };
-  toggleVideo = () => {
-    const command = this.state.showVideo ? 'pauseVideo' : 'playVideo';
-    this.setState({ showVideo: !this.state.showVideo });
-    this.iframe.contentWindow.postMessage(`{"event":"command","func":"${command}","args":""}`, '*');
-  };
-  render = () => (
-    <SSection id="hero" minHeight={860} color={colors.fadedBlue} background={<SBackgroundImage />}>
-      <SSectionWrapper>
-        <SPreviewWrapper onClick={this.toggleVideo} mobile>
-          <SPreview mobile img={previewMobile}>
-            <SPlayButton mobile />
-          </SPreview>
-        </SPreviewWrapper>
+          <SHeroTitle>Get updates on Balance product releases</SHeroTitle>
+          <STagline>We're building a wallet for all the world's currencies, available soon for Mac and iOS.</STagline>
+        </SSubscribe>
+      </SHalf>
 
-        <SHalf>
-          <SSubscribe>
-            <SFormWrapper>
-              <img src={emailIcon} alt="email" />
-              <SForm />
-            </SFormWrapper>
+      <SPreviewWrapper onClick={toggleVideo}>
+        <SPreview img={previewDesktop}>
+          <SPlayButton />
+        </SPreview>
+      </SPreviewWrapper>
+    </SSectionWrapper>
+  </SSection>
+);
 
-            <SHeroTitle>Get updates on Balance product releases</SHeroTitle>
-            <STagline>We're building a wallet for all the world's currencies, available soon for Mac and iOS.</STagline>
-          </SSubscribe>
-        </SHalf>
-
-        <SPreviewWrapper onClick={this.toggleVideo}>
-          <SPreview img={previewDesktop}>
-            <SPlayButton />
-          </SPreview>
-        </SPreviewWrapper>
-
-        <SVideoContainer show={this.state.showVideo} onClick={this.toggleVideo}>
-          <SVideoWrapper>
-            <iframe
-              ref={c => (this.iframe = c)}
-              title="wefunder-youtube"
-              src={
-                typeof window !== 'undefined' ? window.innerWidth > 640 ? (
-                  'https://www.youtube.com/embed/05w-S5gY0Y4?enablejsapi=1&showinfo=0&rel=0&color=white'
-                ) : (
-                  'https://www.youtube.com/embed/c4UGoACmhUE?enablejsapi=1&showinfo=0&rel=0&color=white'
-                ) : null
-              }
-              allowFullScreen
-              frameBorder="0"
-            />
-          </SVideoWrapper>
-        </SVideoContainer>
-      </SSectionWrapper>
-    </SSection>
-  );
-}
+Hero.propTypes = {
+  toggleVideo: PropTypes.func.isRequired
+};
 
 export default Hero;
