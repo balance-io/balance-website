101f72314a4314c505dc36f3436189cd8acd701d
Pedro Gomes <pedrogomes94@gmail.com>
2018-02-16 16:35:35 +0000

    add blog/post pages

---
+++ src/pages/blog.js
@@ -0,0 +1,297 @@
+import React from 'react';
+import styled from 'styled-components';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
+import Section from '../components/Section';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import blogTriangles from '../assets/blog-directory-triangles.svg';
+import mediumLogo from '../assets/medium-logo.svg';
+import { colors, transitions, responsive } from '../styles';
+import { ellipseText, getTimeagoString } from '../utils/helpers';
+
+const SBlog = styled(Section)`
+  padding: 48px 0 12px;
+`;
+
+const STriangles = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 132px;
+  height: 144px;
+  display: none;
+  pointer-events: none;
+  background: url(${blogTriangles}) no-repeat;
+  @media screen and (${responsive.sm.max}) {
+    display: block;
+  }
+`;
+
+const dividerColors = [
+  '#A539BD',
+  '#9251AC',
+  '#32325D',
+  '#3079C0',
+  '#217AB7',
+  '#00AEA5',
+  '#517299',
+  '#54606C'
+];
+
+const SDivider = styled.div`
+  position: relative;
+  width: 100%;
+  height: 4px;
+  overflow: hidden;
+  margin-bottom: 18px;
+  display: ${({ i }) => (i === 0 ? 'none' : 'block')};
+  background: rgb(${colors.dark});
+  @media screen and (${responsive.sm.max}) {
+    width: 110%;
+    margin-left: -5%;
+  }
+  & > div {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 1;
+    transition: ${transitions.short};
+    background: ${({ i }) => dividerColors[i % 8]};
+  }
+`;
+
+const SPostCards = styled.div`
+  display: block;
+  box-sizing: border-box;
+  position: relative;
+  margin-bottom: 36px;
+  width: 100%;
+  cursor: pointer;
+  transition: all 0.38s cubic-bezier(0.19, 1, 0.22, 1);
+  overflow: hidden;
+  & p,
+  & h2 {
+    padding-right: ${({ medium }) => (medium ? '50px' : 0)};
+  }
+  @media screen and (${responsive.sm.max}) {
+    border-radius: 0;
+    padding: 0 4%;
+  }
+  @media (hover: hover) {
+    &:hover > div > div {
+      opacity: 0;
+      -webkit-filter: blur(6px);
+    }
+  }
+  ${({ i }) => {
+    if (i === 0) {
+      return `	
+      margin-bottom: 32px;	
+      padding: 20px 22px 27px 22px;	
+      margin-left: -22px;	
+      width: calc(100% + 44px);	
+      background: #fff;	
+      border-radius: 5px;	
+      z-index: 5;	
+      box-shadow: 0 0 1px 0 rgba(49,49,93,.12), 0 5px 15px 0 rgba(49,49,93,.1), 0 15px 35px 0 rgba(49,49,93,0.15), 0 50px 100px 0 rgba(49,49,93,0.1);	
+      @media screen and (${responsive.sm.max}) {	
+        transform: scale(.96) rotate3d(0,0,0,0);	
+      	border-radius: 5px;	
+        padding: 20px 8% 27px;	
+      }	
+      &:active {	
+        transform: scale(.92) rotate3d(0,0,0,0);	
+    		border-radius: 6px;	
+      }	
+      `;
+    }
+  }};
+`;
+
+const SPostInfo = styled.p`
+  margin-bottom: 5px;
+  font-size: 0.75em;
+  font-weight: 500;
+  color: rgb(${colors.darkGrey});
+  letter-spacing: 0.2px;
+  text-transform: uppercase;
+`;
+
+const SPostTitle = styled.h2`
+  margin-bottom: 7px;
+  font-weight: 700;
+  letter-spacing: -0.5px;
+  line-height: 1.04;
+`;
+
+const SPostSummary = styled.p`
+  font-family: 'FreightText';
+  font-size: 1.125em;
+  line-height: 1.3333333333;
+  font-weight: 400;
+  font-style: normal;
+  font-stretch: normal;
+  ${({ i }) => {
+    if (i === 0) {
+      return `margin-bottom: 7px;`;
+    }
+  }};
+`;
+
+const SMediumLogo = styled.img`
+  position: absolute;
+  height: 44px;
+  width: 44px;
+  right: 5px;
+  bottom: 0;
+`;
+
+const mergePosts = (contentful, medium) => {
+  const allPosts = contentful.concat(medium);
+  const parsedPosts = allPosts.map(post => {
+    const result = {
+      id: '',
+      slug: '',
+      date: '',
+      title: '',
+      excerpt: '',
+      readingTime: '',
+      medium: false
+    };
+    result.id = post.node.id;
+    result.slug = post.node.slug;
+    result.date = isNaN(post.node.date) ? Date.parse(post.node.date) : Number(post.node.date);
+    if (typeof post.node.title === 'string') {
+      result.medium = true;
+      result.title = post.node.title;
+    } else {
+      result.title = post.node.title.title;
+    }
+    if (post.node.body) {
+      result.excerpt = post.node.body.content.excerpt;
+      result.readingTime = Math.ceil(post.node.body.content.readingTime);
+    } else {
+      result.excerpt = post.node.virtuals.excerpt;
+      result.readingTime = Math.ceil(post.node.virtuals.readingTime);
+    }
+
+    return result;
+  });
+  parsedPosts.sort((a, b) => {
+    if (a.date > b.date) return -1;
+    if (a.date < b.date) return 1;
+    return 0;
+  });
+  return parsedPosts;
+};
+
+const layoutTheme = {
+  fontWeight: '400',
+  linkColor: colors.dark,
+  linkHover: colors.green,
+  mobileToggleColor: colors.dark,
+  mobileToggleOpacity: '0.8',
+  logoColor: colors.dark,
+  logoHover: colors.green
+};
+
+const Blog = ({ data, errors }) => {
+  const contentful = data.allContentfulPost.edges;
+  const medium = data.allMediumPost.edges;
+  const posts = mergePosts(contentful, medium);
+  return (
+    <div>
+      <Helmet title={`${data.site.siteMetadata.title} Blog`} />
+      <Header theme={layoutTheme} />
+      <SBlog maxWidth={700} fontColor={colors.dark}>
+        <STriangles />
+        {posts.map((post, idx) => {
+          if (post.medium) {
+            return (
+              <a key={post.id} href={`https://medium.com/balancemymoney/${post.slug}-${post.id}`}>
+                <SPostCards medium={post.medium} i={idx}>
+                  <SDivider i={idx}>
+                    <div />
+                  </SDivider>
+                  <SPostInfo i={idx}>{`${getTimeagoString(post.date, true)}  •  ${
+                    post.readingTime
+                  } min read`}</SPostInfo>
+                  <SPostTitle>{post.title}</SPostTitle>
+                  <SPostSummary>
+                    {idx > 0 ? ellipseText(post.excerpt, 120) : ellipseText(post.excerpt, 240)}
+                  </SPostSummary>
+                  <SMediumLogo src={mediumLogo} alt="medium" />
+                </SPostCards>
+              </a>
+            );
+          } else {
+            return (
+              <Link key={post.id} to={`blog/${post.slug}`}>
+                <SPostCards medium={post.medium} i={idx}>
+                  <SDivider i={idx}>
+                    <div />
+                  </SDivider>
+                  <SPostInfo i={idx}>{`${getTimeagoString(post.date, true)}  •  ${
+                    post.readingTime
+                  } min read`}</SPostInfo>
+                  <SPostTitle>{post.title}</SPostTitle>
+                  <SPostSummary>
+                    {idx > 0 ? ellipseText(post.excerpt, 120) : ellipseText(post.excerpt, 240)}
+                  </SPostSummary>
+                </SPostCards>
+              </Link>
+            );
+          }
+        })}
+      </SBlog>
+      <Footer theme={layoutTheme} />
+    </div>
+  );
+};
+
+export default Blog;
+
+export const query = graphql`
+  query BlogQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMediumPost {
+      edges {
+        node {
+          id
+          slug
+          date: firstPublishedAt
+          title
+          virtuals {
+            excerpt: subtitle
+            readingTime
+          }
+        }
+      }
+    }
+    allContentfulPost(sort: { fields: [date], order: DESC }) {
+      edges {
+        node {
+          id
+          slug
+          date
+          title {
+            title
+          }
+          body {
+            content: childMarkdownRemark {
+              excerpt(pruneLength: 250)
+              readingTime: timeToRead
+            }
+          }
+        }
+      }
+    }
+  }
+`;
---
+++ src/templates/post.js
@@ -0,0 +1,247 @@
+import React from 'react';
+import styled from 'styled-components';
+import Helmet from 'react-helmet';
+import Section from '../components/Section';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { colors, responsive } from '../styles';
+import { getTimeagoString } from '../utils/helpers';
+import TrianglesLeft from '../assets/blog-triangles-left.svg';
+import TrianglesRight from '../assets/blog-triangles-right.svg';
+
+const SBackgroundTriangles = styled.div`
+  @media screen and (${responsive.sm.max}) {
+    display: none;
+  }
+`;
+
+const STrianglesLeft = styled.div`
+  position: absolute;
+  top: 0;
+  z-index: -1;
+  left: 0;
+  width: 300px;
+  height: 450px;
+  background: url(${TrianglesLeft}) no-repeat;
+`;
+
+const STrianglesRight = styled.div`
+  position: absolute;
+  top: 0;
+  z-index: -1;
+  right: 0;
+  width: 400px;
+  height: 300px;
+  background: url(${TrianglesRight}) no-repeat;
+`;
+
+const SBackground = () => (
+  <SBackgroundTriangles>
+    <STrianglesLeft />
+    <STrianglesRight />
+  </SBackgroundTriangles>
+);
+
+const SPost = styled(Section)`
+  padding: 56px 0 12px;
+  & h1 {
+    margin-bottom: 11px;
+    font-size: 2.5em;
+    font-weight: 700;
+    letter-spacing: -1px;
+    line-height: 1.04;
+  }
+
+  & h2 {
+    margin: 48px 0 11px 0;
+    font-size: 1.75em;
+    font-weight: 700;
+    letter-spacing: -0.6px;
+    line-height: 1.04;
+  }
+  @media screen and (${responsive.sm.max}) {
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+`;
+
+const SInfo = styled.div`
+  margin-bottom: 29px;
+  height: 60px;
+  & p {
+    font-size: 0.8125em;
+    color: rgb(${colors.darkGrey});
+  }
+  & p:first-of-type {
+    margin-bottom: 4px;
+  }
+`;
+
+const SAuthorImage = styled.div`
+  float: left;
+  margin-right: 15px;
+  width: 60px;
+  height: 60px;
+  background-size: 60px;
+  border-radius: 50%;
+  overflow: hidden;
+  box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.06);
+  & img {
+    width: 100%;
+  }
+`;
+
+const SAuthorName = styled.h5`
+  padding-top: 2px;
+  margin-bottom: 3px;
+  font-size: 0.9375em;
+  letter-spacing: -0.1px;
+  font-weight: 500;
+`;
+
+const SPostContent = styled.div`
+  & p {
+    margin-bottom: 29px;
+    font-family: 'FreightText';
+    font-size: 1.3125em;
+    line-height: 1.5714285714;
+  }
+  & a {
+    display: inline-block;
+    font-weight: 400;
+    background-image: linear-gradient(
+      to bottom,
+      rgba(51, 51, 51, 0.75) 50%,
+      rgba(51, 51, 51, 0) 50%
+    );
+    background-repeat: repeat-x;
+    background-size: 2px 0.1em;
+    background-position: 0 1.25em;
+  }
+  & a:active {
+    -webkit-transform: scale(0.95) translate3d(0, 0, 0);
+    -webkit-transition: 0.06s ease;
+  }
+`;
+
+const layoutTheme = {
+  fontWeight: '400',
+  linkColor: colors.green,
+  linkHover: colors.dark,
+  mobileToggleColor: colors.green,
+  mobileToggleOpacity: '1',
+  logoColor: colors.green,
+  logoHover: colors.dark
+};
+
+const Post = ({ data }) => {
+  const siteTitle = data.site.siteMetadata.title;
+  const siteUrl = data.site.siteMetadata.baseUrl;
+
+  const post = data.contentfulPost;
+
+  const title = post.title.title;
+  const date = post.date;
+  const category = post.category.title;
+  const tags = post.tags;
+  const slug = post.slug;
+  const excerpt = post.body.content.excerpt;
+  const featuredImage = `https:${post.featuredImage.file.url}`;
+  const readingTime = post.body.content.timeToRead;
+  const html = post.body.content.html;
+
+  const authorName = post.author[0].name;
+  const authorBio = post.author[0].biography.biography;
+  const authorImg = post.author[0].profilePhoto.file.url;
+  return (
+    <div>
+      <Helmet
+        title={`${title} - ${siteTitle} Blog`}
+        meta={[
+          { name: 'description', content: excerpt },
+          { name: 'keywords', content: tags },
+
+          { name: 'twitter:title', content: title },
+          { name: 'twitter:card', content: 'summary_large_image' },
+          { name: 'twitter:description', content: excerpt },
+          { name: 'twitter:image:src', content: featuredImage },
+
+          { name: 'og:title', content: title },
+          { name: 'og:type', content: 'article' },
+          { name: 'og:url', content: `${siteUrl}/post/${slug}` },
+          { name: 'og:description', content: excerpt },
+          { name: 'og:image', content: featuredImage },
+
+          { name: 'article:published_time', content: date },
+          { name: 'article:author', content: authorName },
+          { name: 'article:section', content: category }
+        ]}
+      />
+      <Header theme={layoutTheme} />
+      <SPost maxWidth={700} fontColor={colors.dark} background={<SBackground />}>
+        <header>
+          <SInfo>
+            <SAuthorImage>
+              <img src={authorImg} alt={authorName} />
+            </SAuthorImage>
+            <SAuthorName>{authorName}</SAuthorName>
+            <p>{authorBio}</p>
+            <p>{`Posted ${getTimeagoString(date, true)}  •  ${readingTime} min read`}</p>
+          </SInfo>
+          <h1>{title}</h1>
+        </header>
+        <article>
+          <SPostContent dangerouslySetInnerHTML={{ __html: html }} />
+        </article>
+      </SPost>
+      <Footer theme={layoutTheme} />
+    </div>
+  );
+};
+
+export default Post;
+
+export const query = graphql`
+  query PostQuery($slug: String) {
+    site {
+      siteMetadata {
+        title
+        baseUrl
+      }
+    }
+    contentfulPost(slug: { eq: $slug }) {
+      date
+      category {
+        title
+      }
+      tags
+      slug
+      featuredImage {
+        file {
+          url
+        }
+      }
+      author {
+        name
+        biography {
+          biography
+        }
+        profilePhoto {
+          file {
+            url
+          }
+        }
+      }
+      title {
+        title
+      }
+      body {
+        content: childMarkdownRemark {
+          excerpt
+          timeToRead
+          html
+        }
+      }
+    }
+  }
+`;
