b3ac0354fb049074ecd6191dd47f14c950b2e33b
Jeff Reiner <theundeadghost@gmail.com>
2019-02-18 17:51:48 -0500

    :sparkles: updating theme color stuff

--- v2/src/components/Layout/index.js
+++ v2/src/components/Layout/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Box } from "rebass";
 
 import "modern-normalize";
 
 const Layout = ({ children }) => (
-  <Box bg="#F8F8FA">
+  <>
     {children}
     <footer>© Balance</footer>
-  </Box>
+  </>
 );
 
 Layout.propTypes = {
