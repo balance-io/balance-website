cde2937f0c5e7a15f0ed0e1e987ccf181549d5be
Jeff Reiner <theundeadghost@gmail.com>
2019-02-18 13:02:27 -0500

    :sparkles: updating favicon

--- v2/gatsby-config.js
+++ v2/gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#fff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/balance-icon.png`
       }
     }
   ]
