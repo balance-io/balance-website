dfb1c5d1d410f9c98f5d1a88879f1b1e866df51c
Jeff Reiner <theundeadghost@gmail.com>
2019-02-18 12:54:59 -0500

    :sparkles: adding lambda deps n basis of it

--- v2/functions/getMailchimpSubscribers.js
+++ v2/functions/getMailchimpSubscribers.js
@@ -1,8 +1,23 @@
-export async function handler(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello world ${Math.floor(Math.random() * 10)}`
-    })
-  };
-}
+import got from "got";
+import dotenv from "dotenv";
+dotenv.config();
+
+exports.handler = async (event, context) => {
+  try {
+    const response = await got(`/lists/3985713da6`, {
+      baseUrl: `https://us11.api.mailchimp.com/3.0`,
+      headers: {
+        Authorization: `apikey ${process.env.MAILCHIMP_API_KEY}`
+      }
+    }).json();
+
+    console.log(response);
+
+    return {
+      statusCode: 200,
+      body: response.body
+    };
+  } catch (error) {
+    console.log("Error: ", error.response.body);
+  }
+};
