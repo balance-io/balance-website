73076a02c787264692e13eaa3563d2df9d604a1e
Jeff Reiner <theundeadghost@gmail.com>
2019-02-18 12:19:46 -0500

    :sparkles: adding Lambda beginnings

---
+++ v2/functions/getMailchimpSubscribers.js
@@ -0,0 +1,8 @@
+export async function handler(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello world ${Math.floor(Math.random() * 10)}`
+    })
+  };
+}
