6540c17994021ee8b20cd727fff8f14de7b15ff2
Jeff Reiner <theundeadghost@gmail.com>
2019-02-21 17:24:48 -0500

    :sparkles: updating container component

--- v2/src/components/Container/index.js
+++ v2/src/components/Container/index.js
@@ -1,12 +1,17 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 import { Box } from "rebass";
 
-const Container = styled(Box)({
-  maxWidth: 960
-});
+const Container = styled(Box)`
+  max-width: ${props => (props.maxWidth ? `${props.maxWidth}px` : "960px")};
+`;
 
 Container.defaultProps = {
   mx: "auto"
+};
+
+Container.propTypes = {
+  maxWidth: PropTypes.number
 };
 
 export default Container;
