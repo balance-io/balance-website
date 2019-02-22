1e867c30c0c34067efc1529d8b38418d0500cbb0
Jeff Reiner <theundeadghost@gmail.com>
2019-02-21 19:09:34 -0500

    :sparkles: cleaning up components

--- v2/src/pages/index.js
+++ v2/src/pages/index.js
@@ -8,10 +8,11 @@ import SEO from "../components/SEO";
 import Header from "../components/Header";
 import Waves from "../components/Waves";
 
-import tweets from "../data/tweets.json";
 import Footer from "../components/Footer";
 import Integration from "../components/Integration";
+import Subscribers from "../components/Subscribers";
 
+import tweets from "../data/tweets.json";
 import integrations from "../data/integrations.json";
 
 import device from "../images/iphone.png";
@@ -53,28 +54,34 @@ const IndexPage = () => (
           <Waves />
 
           <Container>
-            <Flex justifyContent="center">
-              <Image
-                src={device}
-                css={`
-                  vertical-align: middle;
-                `}
-              />
-            </Flex>
+            <Image
+              src={device2x}
+              my={5}
+              mx="auto"
+              css={`
+                display: block;
+                max-width: 320px;
+                width: 100%;
+                vertical-align: middle;
+              `}
+            />
           </Container>
         </Box>
+
         {/* Cards */}
         <Flex>
           {integrations.map((integration, index) => (
             <Integration key={index} {...integration} />
           ))}
         </Flex>
+
+        {/* App Store */}
         <Container>
-          {/* App Store */}
           <Flex justifyContent="center" my={5}>
             <Link href="https://testflight.apple.com/join/QXCgM6bu">
               <Image
                 css={`
+                  height: 56px;
                   vertical-align: middle;
                 `}
                 src={badge}
@@ -85,37 +92,49 @@ const IndexPage = () => (
       </Box>
 
       <Box bg="white" py={4}>
+        {/* Community */}
         <Container>
-          {/* Community */}
           <Box mb={[4, 5]}>
             <Heading textAlign="center" fontWeight={2} fontSize={3}>
               Join the <strong>Balance Community</strong>
             </Heading>
 
-            <Card
-              p={1}
-              width={1 / 3}
-              borderRadius={6.4}
-              border="1px solid #d6d6d6"
-              bg="#fafbfc"
-              boxShadow="0 2px 3px 0 rgba(241, 241, 241, 0.5)"
-            >
-              <Flex justifyContent="space-between">
-                <input placeholder="your@email.com" type="email" />
-                <Button variant="foam">Join Us</Button>
-              </Flex>
-            </Card>
+            <Flex justifyContent="center" my={3}>
+              <Card
+                p={1}
+                width={1 / 3}
+                borderRadius={6.4}
+                border="1px solid #d6d6d6"
+                bg="#fafbfc"
+                boxShadow="0 2px 3px 0 rgba(241, 241, 241, 0.5)"
+              >
+                <Flex justifyContent="space-between">
+                  <input placeholder="your@email.com" type="email" />
+                  <Button variant="foam">Join Us</Button>
+                </Flex>
+              </Card>
+            </Flex>
+
+            <Subscribers />
           </Box>
         </Container>
 
         {/* Tweets */}
         <Container maxWidth={1136}>
           <Text fontSize={3} textAlign="center" as="p" mt={0} mb={4}>
-            Follow <Link color="deepSkyBlue" href="">@balance_io</Link> on Twitter
+            Follow{" "}
+            <Link color="deepSkyBlue" href="">
+              @balance_io
+            </Link>{" "}
+            on Twitter
           </Text>
           <Box>
-            {tweets.map(tweet => (
-              <TwitterTweetEmbed tweetId={tweet} />
+            {tweets.map((tweet, index) => (
+              <TwitterTweetEmbed
+                key={index}
+                options={{ height: 250 }}
+                tweetId={tweet}
+              />
             ))}
           </Box>
         </Container>
