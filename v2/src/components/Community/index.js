82e902d205751784a7513748ecd3daa3d4c070a7
Jeff Reiner <theundeadghost@gmail.com>
2019-02-22 08:22:11 -0500

    :recycle: moving subbscribers into Community section

--- v2/src/components/Community/index.js
+++ v2/src/components/Community/index.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
-import { Heading, Card, Flex, Button } from "rebass";
+import React, { useState, useEffect } from "react";
+import { Heading, Card, Flex, Button, Text } from "rebass";
 import addToMailchimp from "gatsby-plugin-mailchimp";
 import { useInput } from "react-hanger";
 
-import Subscribers from "../Subscribers";
+const Subscribers = () => {
+  const [subscribers, setSubscribers] = useState(0);
+
+  const fetchSubscribers = () =>
+    fetch("/.netlify/functions/getMailchimpSubscribers")
+      .then(res => res.json())
+      .then(json => setSubscribers(json.stats.member_count));
+
+  useEffect(() => {
+    fetchSubscribers();
+  }, []);
+
+  return (
+    <Text fontSize={0} color="textLight" textAlign="center">
+      {subscribers.toLocaleString()} subscribers
+    </Text>
+  );
+};
 
 const Community = () => {
   const email = useInput("");
