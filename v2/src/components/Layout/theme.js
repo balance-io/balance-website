5cdf0dbbf4484215735724226778477cb2fb1257
Jeff Reiner <theundeadghost@gmail.com>
2019-02-24 17:04:41 -0500

    :rotating_light: fixing linter errros

--- v2/src/components/Layout/theme.js
+++ v2/src/components/Layout/theme.js
@@ -26,9 +26,8 @@ export default {
       background: "#FFFFFF",
       border: "0.5px solid #E9E9E9",
       fontSize: props => (props.fontSize ? props.fontSize : 14),
-      fontWeight: "normal",
+      fontWeight: 500,
       color: "#0179FF",
-      fontWeight: 400,
       boxShadow:
         "0 1px 2px 0 rgba(206,206,206,0.50), inset 0 -2px 2px 0 rgba(237,237,237,0.50)",
       borderRadius: 4.8
