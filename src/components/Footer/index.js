42df2cd12f76f3cc02b4c02040344cf497cf7284
Jeff Reiner <theundeadghost@gmail.com>
2019-03-25 13:06:01 +0100

    :recycle: refactor footer to include icons and be mobile good

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,23 +1,62 @@
 import React from "react";
-import { Flex } from "rebass";
+import { Flex, Image } from "rebass";
 
 import { ExternalLink } from "../Links";
 
 import { useSiteMetadata } from "../../hooks";
 
+import blog from "../../images/icons/blog.svg";
+import calendar from "../../images/icons/calendar.svg";
+import discord from "../../images/icons/discord.svg";
+import github from "../../images/icons/github.svg";
+import twitter from "../../images/icons/twitter.svg";
+import forum from "../../images/icons/forum.svg";
+
+const FooterIcon = ({ label, ...rest }) => {
+  const getIcon = label => {
+    switch (label) {
+      case "GitHub":
+        return github;
+      case "Twitter":
+        return twitter;
+      case "Discord":
+        return discord;
+      case "Forum":
+        return forum;
+      case "Blog":
+        return blog;
+      case "Meetups":
+      default:
+        return calendar;
+    }
+  };
+
+  return <Image src={getIcon(label)} {...rest} />;
+};
+
 const Footer = ({ light }) => {
   const { urls } = useSiteMetadata();
 
   return (
-    <Flex as="footer" justifyContent="space-evenly" alignItems="center">
+    <Flex
+      as="footer"
+      flexDirection={["column", "row"]}
+      justifyContent="space-evenly"
+      alignItems="center"
+    >
       {urls.map((link, index) => (
         <ExternalLink
           color={light ? "textLighter" : "textDark"}
           fontSize={1}
           key={index}
           href={link.url}
+          mt={[index !== 0 ? 3 : 0, 0]}
+          py={2}
         >
-          {link.label}
+          <Flex>
+            <FooterIcon mr={2} label={link.label} />
+            {link.label}
+          </Flex>
         </ExternalLink>
       ))}
     </Flex>
